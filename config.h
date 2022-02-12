a602116a0d45ecf4230d45770529ec8f39c8636a
Rafael Belo Duarte <rafaelbeloduarte@protonmail.com>
2022-02-12 13:17:23 -0300

    cor da barra

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Liberation Mono:size=14" };
 static const char dmenufont[]       = "Liberation Mono:size=14";
-static const char col_gray1[]       = "#2f343f";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
